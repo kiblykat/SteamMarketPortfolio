cab337f87623c8a286117b0a931940210220b671
Izzat Fadzlon <muhammad.izzat.2.fadzlon@continental-corporation.com>
2025-02-23 19:24:55 +0800

    Fix schema definition for steamItem field in Transaction model

--- backend/src/models/Transaction.ts
+++ backend/src/models/Transaction.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
   uid: { type: String, required: true },
-  steamItem: { type: "String", required: true },
+  steamItem: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
   date: { type: Date, default: Date.now },
