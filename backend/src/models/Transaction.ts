1ed00384f8684f721dae4435af627c88acf5bf51
kiblykat <izzatfadzlon@gmail.com>
2025-02-23 13:09:07 +0800

    be: Add Transaction model with schema for handling transactions

---
+++ backend/src/models/Transaction.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema({
+  uid: { type: String, required: true },
+  steamItem: { type: "String", required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+export const transactionModel = mongoose.model(
+  "Transaction",
+  transactionSchema
+);
