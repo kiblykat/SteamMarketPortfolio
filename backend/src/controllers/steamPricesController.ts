d83869e12221ebf03a9fcf60226eaf226b5cfd59
izzat <izzatfadzlon@gmail.com>
2025-02-25 12:08:59 +0800

    steamPricesController: extend getCasePrices to include highest buy orders for prismaCase and clutchCase

--- backend/src/controllers/steamPricesController.ts
+++ backend/src/controllers/steamPricesController.ts
@@ -6,9 +6,17 @@ export const getCasePrices = async (req: Request, res: Response) => {
     const fractureCaseResponse = await axios.get(
       "https://steamcommunity.com/market/itemordershistogram?country=SG&language=english&currency=13&item_nameid=176185874"
     );
-    res
-      .status(200)
-      .json({ fractureCase: fractureCaseResponse.data.highest_buy_order }); // highest_buy_order is the price of the most expensive buy order
+    const prismaCaseResponse = await axios.get(
+      "https://steamcommunity.com/market/itemordershistogram?country=SG&language=english&currency=13&item_nameid=176042493"
+    );
+    const clutchCaseResponse = await axios.get(
+      "https://steamcommunity.com/market/itemordershistogram?country=SG&language=english&currency=13&item_nameid=175966708"
+    );
+    res.status(200).json({
+      fractureCase: fractureCaseResponse.data.highest_buy_order,
+      prismaCase: prismaCaseResponse.data.highest_buy_order,
+      clutchCase: clutchCaseResponse.data.highest_buy_order,
+    }); // highest_buy_order is the price of the most expensive buy order
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
