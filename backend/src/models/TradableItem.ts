6bf1056782805dea7708002737874541767b6225
izzat <izzatfadzlon@gmail.com>
2025-04-04 15:57:23 +0800

    be TradableItem: add imageUrl field to TradableItem schema

--- backend/src/models/TradableItem.ts
+++ backend/src/models/TradableItem.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const TradableItemSchema = new mongoose.Schema({
   itemName: { type: String, required: true }, // Group items by name
+  imageUrl: { type: String }, // Url of item's image
   releaseDate: { type: Date }, // Release date of the item
 });
 
