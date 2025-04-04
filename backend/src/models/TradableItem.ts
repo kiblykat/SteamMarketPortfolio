1199e1b21f2a53df6ff8fd101320ee2c3d3aeeac
izzat <izzatfadzlon@gmail.com>
2025-04-04 17:49:07 +0800

    be TradableItem: make itemName field unique in TradableItem schema

--- backend/src/models/TradableItem.ts
+++ backend/src/models/TradableItem.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const TradableItemSchema = new mongoose.Schema({
-  itemName: { type: String, required: true }, // Group items by name
+  itemName: { type: String, required: true, unique: true }, // Group items by name
   imageUrl: { type: String }, // Url of item's image
   releaseDate: { type: Date }, // Release date of the item
 });
