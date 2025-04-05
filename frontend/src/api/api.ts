af01d379d0153e53b206716d65d8eea07bfefae8
izzat <izzatfadzlon@gmail.com>
2025-04-05 14:09:34 +0800

    fe(api): update BASE_URL to production endpoint

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// let BASE_URL = "https://leetspace-api.vercel.app";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://steam-market-portfolio-backend.vercel.app/";
+// const BASE_URL = "http://localhost:5000";
 const transactionAPI = axios.create({ baseURL: BASE_URL });
 
 export default transactionAPI;
