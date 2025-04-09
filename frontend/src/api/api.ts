f97354605ac40c5479e092545db4154422d7a410
izzat <izzatfadzlon@gmail.com>
2025-04-09 13:04:16 +0800

    fe api: update BASE_URL to use environment variable for backend URL

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://steam-market-portfolio-backend.vercel.app/";
+const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 // const BASE_URL = "http://localhost:5000";
 const transactionAPI = axios.create({ baseURL: BASE_URL });
 
