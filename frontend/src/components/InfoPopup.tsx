bc4da6b49f6e393f36d33b7647b93bf9c628686a
izzat <izzatfadzlon@gmail.com>
2025-04-09 20:09:36 +0800

    fe InfoPopup: refactor component to use TypeScript interface for props and fix failed production

--- frontend/src/components/InfoPopup.tsx
+++ frontend/src/components/InfoPopup.tsx
@@ -1,15 +1,28 @@
-const InfoPopup = ({ setPopupVisible, popupVisible }) => {
+interface InfoPopupProps {
+  setPopupVisible: (visible: boolean) => void;
+  popupVisible: boolean;
+}
+
+const InfoPopup = ({ setPopupVisible }: InfoPopupProps) => {
   return (
     <div
       onClick={() => setPopupVisible(false)}
       className="z-40 fixed inset-0 bg-black bg-opacity-50"
     >
       <div
-        className="fixed inset-32 bg-white rounded-lg shadow-lg"
+        className="fixed bg-white rounded-lg shadow-lg"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="text-xl">
-          {popupVisible ? "popupVisible" : "no popupVisible"}
+          <div className="flex justify-between items-center p-4">
+            <span>Info</span>
+            <button
+              className="text-gray-500 hover:text-gray-700"
+              onClick={() => setPopupVisible(false)}
+            >
+              &times;
+            </button>
+          </div>
         </div>
       </div>
     </div>
