2cd46db8b7105bce2d401848c48fabf10339391e
izzat <izzatfadzlon@gmail.com>
2025-04-11 15:20:11 +0800

    cron: move cron config from frontend to backend

---
+++ backend/src/cron/retrievePL.ts
@@ -0,0 +1,9 @@
+// import transactionAPI from "../api/api";
+
+export const fillPLData = async () => {
+  try {
+    console.log("placeholder for PL data retrieval");
+  } catch (error) {
+    console.error("Error fetching PL data:", error);
+  }
+};
--- frontend/src/cron/retrievePL.ts
+++
@@ -1,9 +0,0 @@
-// import transactionAPI from "../api/api";
-
-export const fillPLData = async () => {
-  try {
-    console.log("placeholder for PL data retrieval");
-  } catch (error) {
-    console.error("Error fetching PL data:", error);
-  }
-};
