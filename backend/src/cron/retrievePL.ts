9cc219c6c9de0129df24188c4d57aefbdd0bff42
izzat <izzatfadzlon@gmail.com>
2025-04-12 14:28:45 +0800

    Move retrievePL API endpoint to correct path in vercel.json and remove obsolete cron file

--- backend/src/cron/retrievePL.ts
+++
@@ -1,9 +0,0 @@
-// import transactionAPI from "../api/api";
-
-import mongoose from "mongoose";
-import { connectDB, disconnectDB } from "../../config/db";
-import axios from "axios";
-
-export function GET(request: Request) {
-  return new Response("Hello from Vercel!");
-}
