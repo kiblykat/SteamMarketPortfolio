732ec6865f8b63252592b1d6013f2ca35e858a39
izzat <izzatfadzlon@gmail.com>
2025-04-12 13:10:38 +0800

    retrievePL test: follow quickstart documentation

--- backend/src/cron/retrievePL.ts
+++ backend/src/cron/retrievePL.ts
@@ -4,13 +4,6 @@ import mongoose from "mongoose";
 import { connectDB, disconnectDB } from "../../config/db";
 import axios from "axios";
 
-const fillPLData = async () => {
-  try {
-    connectDB();
-    disconnectDB();
-  } catch (error) {
-    console.error("Error fetching PL data:", error);
-  }
-};
-
-export default fillPLData;
+export function GET(request: Request) {
+  return new Response("Hello from Vercel!");
+}
