44c3de1f6e73be565b130e1af7e7feb5dfb3083c
izzat <izzatfadzlon@gmail.com>
2025-04-14 17:01:55 +0800

    be (portfolioHistoryController): explicit declaration of timestamp within function to ensure all items will ahve same timestamp

--- backend/src/controllers/portfolioHistoryController.ts
+++ backend/src/controllers/portfolioHistoryController.ts
@@ -8,6 +8,7 @@ interface PortfolioWithUID {
   realizedPL: number;
   PL: number;
   uid: string; // Add uid property
+  timestamp: Date;
 }
 
 export const createNewPortfolioHistory = async (
@@ -17,11 +18,16 @@ export const createNewPortfolioHistory = async (
   try {
     const uid = process.env.UID as string; // Assuming UID is set in the environment variables
     const portfolioWithPL = await fetchPortfolio(uid);
-    (portfolioWithPL[1] as PortfolioWithUID[]).map((item) => {
-      item.uid = uid; // Add uid to each portfolio item
-    });
-    await PortfolioHistory.insertMany(portfolioWithPL[1]);
-    res.status(201).json(portfolioWithPL[1]);
+    const timestamp = new Date(); // Get the current timestamp
+    const portfolioWithTimestamp = (
+      portfolioWithPL[1] as PortfolioWithUID[]
+    ).map((item) => ({
+      ...item, // Spread the existing properties
+      uid, // Add uid to each portfolio item
+      timestamp, // Set the current timestamp (ensure all dates within this request are the same)
+    }));
+    await PortfolioHistory.insertMany(portfolioWithTimestamp);
+    res.status(201).json(portfolioWithTimestamp);
   } catch (err) {
     console.error("Error creating past profit:", err);
     res.status(500).json({ error: "Server error occurred." });
